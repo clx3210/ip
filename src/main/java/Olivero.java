14a7c370f9c60e7d083c2981aec0ebb86cc5c847
Chen Linxin <chen_ll@u.nus.edu>
2025-01-15 18:38:13 +0800

    Modularise chatbot messages

--- src/main/java/Olivero.java
+++ src/main/java/Olivero.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
 public class Olivero {
+
+    private static final String SEPARATOR =  "\n____________________________________________________________";
+    private static final String GREETING_MESSAGE = "Howdy-do! I'm Olivero, What can I do for you?";
+    private static final String EXIT_MESSAGE = "Bye-bye. See you soon!";
+    public static void speak(String message) {
+        System.out.println(SEPARATOR);
+        System.out.println(message + "\n");
+        System.out.println(SEPARATOR + "\n");
+    }
     public static void main(String[] args) {
-        String name = "Olivero";
-        System.out.println("---------------------------------------");
-        System.out.println("Howdy-do! I'm " + name);
-        System.out.println("What can I do for you?\n");
-        System.out.println("---------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        speak(GREETING_MESSAGE);
+        speak(EXIT_MESSAGE);
+        // TODO: echo user input until 'bye' is read
+
     }
 }
