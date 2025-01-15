89427d5db22f6962a828fe54099e77d3d1b0a536
Chen Linxin <chen_ll@u.nus.edu>
2025-01-15 18:08:37 +0800

    Add chatbot greeting and exit

--- src/main/java/Olivero.java
+++ src/main/java/Olivero.java
@@ -1,10 +1,10 @@
 public class Olivero {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Olivero";
+        System.out.println("---------------------------------------");
+        System.out.println("Howdy-do! I'm " + name);
+        System.out.println("What can I do for you?\n");
+        System.out.println("---------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
