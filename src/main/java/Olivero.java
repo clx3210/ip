0520a350565e16d07a60d0da568ea4b8eaaff81e
Chen Linxin <chen_ll@u.nus.edu>
2025-01-15 18:40:41 +0800

    Add echo functionality

--- src/main/java/Olivero.java
+++ src/main/java/Olivero.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 public class Olivero {
 
-    private static final String SEPARATOR =  "\n____________________________________________________________";
+    private static final String SEPARATOR =  "____________________________________________________________";
     private static final String GREETING_MESSAGE = "Howdy-do! I'm Olivero, What can I do for you?";
     private static final String EXIT_MESSAGE = "Bye-bye. See you soon!";
     public static void speak(String message) {
-        System.out.println(SEPARATOR);
-        System.out.println(message + "\n");
-        System.out.println(SEPARATOR + "\n");
+        System.out.println("\t" + SEPARATOR);
+        System.out.println("\t" + message + "\n");
+        System.out.println("\t" + SEPARATOR + "\n");
     }
     public static void main(String[] args) {
         speak(GREETING_MESSAGE);
-        speak(EXIT_MESSAGE);
-        // TODO: echo user input until 'bye' is read
-
+        // echo user input until 'bye' is read
+        String endToken = "bye";
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            if (line.equals(endToken)) {
+                speak(EXIT_MESSAGE);
+                break;
+            } else {
+                speak(line);
+            }
+        }
     }
 }
